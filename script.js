b2ddf650e04bb7dfc3f947d89d2451f3068de542
devression <99890933+devression@users.noreply.github.com>
2022-10-20 01:20:25 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
